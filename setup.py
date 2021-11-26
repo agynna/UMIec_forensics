a7ed7bec7773b8bae13e0161c1192e70a696d7f9
Arvid Gynnå <arvid.heden-gynna@polisen.se>
2021-11-26 11:33:12 +0100

    Readme update

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
-install_requires = ["fdstools", 
-                    "matplotlib", 
-                    "pysam"]
+install_requires = ["fdstools>=2",
+                    "matplotlib",
+                    "pysam",
+                    "umierrorcorrect>=0.19"]
 
-setup(name='umierrorcorrect',
+plotting_requires = ["matplotlib",
+                     "pandas",
+                     "seaborn"]
+
+setup(name='umierrorcorrect_forensics',
       description='UMIerrorcorrect Forensics',
       #long_description = open('README.md').read(),
       #url='http://github.com/',
-      author='Froste Svensson',
+      author='Froste Svensson & Arvid H Gynnå',
       author_email='froste.svensson@gmail.com',
       license='mit',
-      package_data={'umierrorcorrect': ['README.md']
+      package_data={'umierrorcorrect_forensics': ['README.md']
                    },
       include_package_data=True,
       install_requires=install_requires,
+      extras_requires={'diagnostics' : plotting_requires}
       classifiers=['Topic :: Scientific/Engineering :: Bio-Informatics'],
-      scripts=['umierrorcorrect_forensics/run_tssv.py'],
+      scripts=['umierrorcorrect_forensics/run_umierrorcorrect_forensics.py'],
       zip_safe=False)
